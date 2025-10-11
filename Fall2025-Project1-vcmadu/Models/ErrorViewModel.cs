306447b15de5861484fa9ac1aba3f500bf4396dc
Vincent Madu <vincentmadu06@outlook.com>
2025-10-10 22:01:02 -0500

    Add Project 1

---
+++ Fall2025-Project1-vcmadu/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fall2025_Project1_vcmadu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# CS 330 Projects (Fall 2025)
+This repository contains all five ASP.NET Core projects for my CS330 course.
+Project 1 is the Teen Wolf showcase site.
