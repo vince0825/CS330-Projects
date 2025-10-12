2f4750033c277356f2fba468ad5a562ac90175be
Vincent Madu <vincentmadu06@outlook.com>
2025-10-12 18:29:33 -0500

    Add Project 2

---
+++ Fall2025-Project2-vcmadu/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fall2025_Project2_vcmadu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
